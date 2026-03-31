31926b8fb3717c235f87209ed2ae0c5192ade31a
fernandoC1905 <fernando.carhuatanta.cabrera@gmail.com>
2026-03-30 19:36:39 -0500

    primer commit

---
+++ Program.py
@@ -0,0 +1,9 @@
+edad : int = 18
+altura : float = 1.8
+nom : str = "Carlos"
+ape = 'Chan'
+esVerde : bool = True
+
+print(f"{nom} {ape} tiene {edad} años y su carro {"es" if esVerde else "no es"} verde")
+
+print(f"su altura es {altura}")
