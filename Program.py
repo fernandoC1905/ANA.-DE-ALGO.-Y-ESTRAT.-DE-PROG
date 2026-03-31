cde42789fcc4d2a8cf2a52e678ca68cf9aa2ae89
fernandoC1905 <fernando.carhuatanta.cabrera@gmail.com>
2026-03-30 19:59:32 -0500

    correccion de x

--- Program.py
+++ Program.py
@@ -1,9 +1,12 @@
 edad : int = 18
-altura : float = 1.8
+altura : float = 1.80
 nom : str = "Carlos"
 ape = 'Chan'
 esVerde : bool = True
+mayorEdad = True
 
 print(f"{nom} {ape} tiene {edad} años y su carro {"es" if esVerde else "no es"} verde")
 
-print(f"su altura es {altura}")
+print(f"su altura es {altura} metros")
+
+print(f"{nom} {ape} {"es" if mayorEdad else "no es"} mayor de edad")
