1a2af84a22e454f7ee2000f0b16c348707ddcf35
fernandoC1905 <fernando.carhuatanta.cabrera@gmail.com>
2026-04-06 19:25:00 -0500

    listas3

---
+++ lista3.py
@@ -0,0 +1,7 @@
+lista = [2 * x  for x in range(1,10 + 1) if x * 2 >= 12]
+print(lista)
+
+print()
+#Multiplo de 3 del 1 al 100
+lista2 = [3 * x for x in range(1,100 + 1) if x * 3 <= 100]
+print(lista2)
